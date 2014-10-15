5ed405ab71462cea01b1cc47e06502c54ee7603f
bala-sankar <balachandar.sankar@live.com>
2014-10-15 11:13:19 -0700

    Pending changes

--- src/main/java/Problems/Problem24.java
+++ src/main/java/Problems/Problem24.java
@@ -10,6 +10,9 @@ import java.util.ArrayList;
  * Date: 12/16/11
  */
 public class Problem24 {
+    private static int count = 0;
+    private static int n = 1000000;
+    private static String result = "";
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
@@ -18,7 +21,7 @@ public class Problem24 {
     }
 
     public static String solution1() {
-        ArrayList<String> lexicographicPermutations;
+        //ArrayList<String> lexicographicPermutations;
         ArrayList<String> numbers = new ArrayList<String>();
         numbers.add("0");
         numbers.add("1");
@@ -30,7 +33,29 @@ public class Problem24 {
         numbers.add("7");
         numbers.add("8");
         numbers.add("9");
-        lexicographicPermutations = Util.getPermutations(numbers);
-        return lexicographicPermutations.get(999999);
+        //lexicographicPermutations = Util.getPermutations(numbers);
+        permutation(numbers, "", numbers.size(), 0);
+        //return lexicographicPermutations.get(999999);
+        return result;
+    }
+
+    protected static void permutation(ArrayList<String> numbers, String num,
+                                      int length, int loop) {
+        if(count == n){
+            return;
+        }
+        if (loop == length) {
+            count++;
+            if(count == n){
+                result = num;
+            }
+            return;
+        }
+        for (String number : numbers) {
+            ArrayList<String> temp = new ArrayList<String>();
+            temp.addAll(numbers);
+            temp.remove(number);
+            permutation(temp, num + number, length, loop + 1);
+        }
     }
 }
